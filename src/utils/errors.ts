5fbdaffb117ee1575be75c913044ca4ca983c50e
liuboheng <lbh9311@sina.com>
2019-09-02 09:28:28 +0800

    add error generator

--- src/utils/errors.ts
+++ src/utils/errors.ts
@@ -18,6 +18,24 @@ export class NoEnoughBalanceError extends Error {
   }
 }
 
+export enum TxResponseCode {
+  unknownError
+}
+
+export const TxResponseInfo = {
+  [TxResponseCode.unknownError]: 'Something wrong!'
+}
+
+export const generateTxResponse = (ifsuccess: boolean, errCode?: number) => {
+  if (ifsuccess) {
+    return { success: true }
+  }
+  if (errCode) {
+    return { success: false, code: errCode, info: TxResponseInfo[errCode] }
+  }
+  return { success: false, code: TxResponseCode.unknownError, info: TxResponseInfo[TxResponseCode.unknownError] }
+}
+
 export default {
   InvalidWalletError,
   NoEnoughBalanceError
