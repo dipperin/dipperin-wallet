9762efee963e8a13a72ab590be38e601bec22a7a
liuboheng <lbh9311@sina.com>
2019-11-08 17:55:28 +0800

    add app ipc communication

--- app/operations/openFile.ts
+++ app/operations/openFile.ts
@@ -2,17 +2,39 @@ import { shell } from 'electron'
 import settings from 'electron-settings'
 import os from 'os'
 import path from 'path'
+import fs from 'fs'
+import fsExtra from 'fs-extra'
+import util from 'util'
 
 import dipperinPath from '../utils/dipperinPath'
 import { getNodeEnv } from './runDipperin'
 
+const fsExists = util.promisify(fs.stat)
+const noop = () => null
+
 export const openTmp = () => {
   const net: string = settings.get('netEnv') as string
-  const chainDataDir = path.join(os.homedir(), `tmp`,`dipperin_apps`,`${getNodeEnv(net)}`,`wallet`)
-  const chainLogPath = path.join(`${chainDataDir}`,`dipperin.log`)
+  const chainDataDir = path.join(os.homedir(), `tmp`, `dipperin_apps`, `${getNodeEnv(net)}`, `wallet`)
+  const chainLogPath = path.join(`${chainDataDir}`, `dipperin.log`)
   shell.showItemInFolder(chainLogPath)
 }
 
 export const openDipperin = () => {
   shell.showItemInFolder(dipperinPath())
+}
+
+export const deleteCSWallet = (net: string) => {
+  const chainDataDir = path.join(os.homedir(), `tmp`, `dipperin_apps`, `${getNodeEnv(net)}`, `wallet`)
+  const csWalletPath = path.join(`${chainDataDir}`, `CSWallet`)
+  fsExists(csWalletPath)
+    .then(() => {
+      fsExtra.remove(csWalletPath)
+    })
+    .catch(noop)
+}
+
+export const getChainDataDir = () => {
+  const net: string = settings.get('netEnv') as string
+  const chainDataDir = path.join(os.homedir(), `tmp`, `dipperin_apps`, `${getNodeEnv(net)}`, `wallet`)
+  return chainDataDir
 }
